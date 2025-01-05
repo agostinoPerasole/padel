0b69c58067dda5deb4e4a7c691bb553a9ac416af
agostinoPerasole <66478012+agostinoPerasole@users.noreply.github.com>
2025-01-05 15:52:13 +0100

    Update Leaderboard.js

--- src/Leaderboard.js
+++ src/Leaderboard.js
@@ -10,7 +10,7 @@ function Leaderboard() {
     // Fetch dei giocatori
     const fetchPlayers = async () => {
       try {
-        const response = await fetch('https://padel-id2x.onrender.com');
+        const response = await fetch('https://padel-id2x.onrender.com/players');
         const data = await response.json();
         setPlayers(data);
       } catch (error) {
@@ -21,7 +21,7 @@ function Leaderboard() {
     // Fetch dei match
     const fetchMatches = async () => {
       try {
-        const response = await fetch('https://padel-id2x.onrender.com');
+        const response = await fetch('https://padel-id2x.onrender.com/matches');
         const data = await response.json();
         setMatches(data);
       } catch (error) {
