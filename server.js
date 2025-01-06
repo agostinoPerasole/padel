65d65c394a6ac658434e2f028640c6fde565923c
Agostino <agop25@gmail.com>
2025-01-06 09:41:27 +0100

    modified server.js

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
+const express = require('express');
+const path = require('path');
 const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // Sostituisci con il percorso corretto del tuo file JSON
-const middlewares = jsonServer.defaults();
 
+const server = express();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 const PORT = process.env.PORT || 5000;
 
+// Middleware per servire i file statici
+server.use(express.static(path.join(__dirname, 'build')));
+
+// Rotta per servire l'app React
+server.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+// Rotte per il JSON Server
 server.use(middlewares);
 server.use(router);
+
 server.listen(PORT, () => {
-    console.log(`JSON Server is running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
